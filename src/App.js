9b70940903ac01e0570e461beb6b940075ea6fc6
vaishnavihole <vaishnavihole1@gmail.com>
2023-10-02 16:47:54 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
+import Card from './Pages/Card';
+
 
 function App() {
   return (
     <div className="App">
-      
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/card' element={<Card/>} />
+    
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
